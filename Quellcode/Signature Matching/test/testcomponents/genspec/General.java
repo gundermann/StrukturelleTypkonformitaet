8dba37736b4b67314252f2cdb52733682756c29b
Niels Gundermann <gundermann.niels.ng@gmail.com>
2020-10-31 10:35:40 +0100

    - Specefic and general test components

---
+++ Quellcode/Signature Matching/test/testcomponents/genspec/General.java
@@ -0,0 +1,33 @@
+package testcomponents.genspec;
+
+public class General {
+
+	private final String stringAttr;
+
+	private int intAttr;
+
+	private Long boxedLongAttr;
+
+	public General(String stringAttr) {
+		this.stringAttr = stringAttr;
+	}
+
+	public General(String stringAttr, int intAttr, Long boxedLongAttr) {
+		this.stringAttr = stringAttr;
+		this.intAttr = intAttr;
+		this.boxedLongAttr = boxedLongAttr;
+	}
+
+	public String getStringAttr() {
+		return stringAttr;
+	}
+
+	public int getIntAttr() {
+		return intAttr;
+	}
+
+	public Long getBoxedLongAttr() {
+		return boxedLongAttr;
+	}
+
+}
---
+++ Quellcode/Signature Matching/test/testcomponents/genspec/Specific.java
@@ -0,0 +1,22 @@
+package testcomponents.genspec;
+
+public class Specific extends General{
+	
+	private final boolean specificAttr;
+	
+	public Specific(String stringAttr) {
+		super(stringAttr);
+		specificAttr = false;
+	}
+	
+	public Specific(String stringAttr, boolean specificAttr) {
+		super(stringAttr);
+		this.specificAttr = specificAttr;
+	}
+
+	public boolean isSpecificAttr() {
+		return specificAttr;
+	}
+
+	
+}
