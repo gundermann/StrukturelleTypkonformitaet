2cb0105880f1f5e83c50e8f26125094d10213d55
Niels Gundermann <gundermann.niels.ng@gmail.com>
2020-11-01 16:11:38 +0100

    tests erweitert

--- Quellcode/Signature Matching/test/glue/GenSpecSignatureMatchingTypeConverterTest.java
+++ Quellcode/Signature Matching/test/glue/GenSpecSignatureMatchingTypeConverterTest.java
@@ -15,11 +15,8 @@ import org.junit.Test;
 
 import matching.methods.MethodMatchingInfo;
 import matching.modules.ModuleMatchingInfo;
-import matching.modules.testmodules.ClassSpec;
-import matching.modules.testmodules.EnumNative;
-import matching.modules.testmodules.InterfaceGen;
-import matching.modules.testmodules.InterfaceWrapper;
 import testcomponents.genspec.DesiredGenInterface;
+import testcomponents.genspec.DesiredSpecInterface;
 import testcomponents.genspec.General;
 import testcomponents.genspec.OfferedSpecClass;
 import testcomponents.genspec.Specific;
@@ -27,7 +24,7 @@ import testcomponents.genspec.Specific;
 public class GenSpecSignatureMatchingTypeConverterTest {
 
 	@Test
-	public void specTargetMatching() throws NoSuchMethodException, SecurityException, IllegalAccessException,
+	public void genSourceSpecTargetMatching() throws NoSuchMethodException, SecurityException, IllegalAccessException,
 			IllegalArgumentException, InvocationTargetException {
 		Class<DesiredGenInterface> source = DesiredGenInterface.class;
 		Class<OfferedSpecClass> target = OfferedSpecClass.class;
@@ -70,42 +67,12 @@ public class GenSpecSignatureMatchingTypeConverterTest {
 		EasyMock.expect(mmiGetLong.getReturnTypeMatchingInfo()).andReturn(createMMI_Long2Long()).anyTimes();
 		EasyMock.expect(mmiGetLong.getArgumentTypeMatchingInfos()).andReturn(createMMIMap(createMMI_G2S())).anyTimes();
 
-//
-//    MethodMatchingInfo mmiDiv = EasyMock.createNiceMock( MethodMatchingInfo.class );
-//    EasyMock.expect( mmiDiv.getTarget() ).andReturn( target.getDeclaredMethod( "div", Double.class, Number.class ) )
-//        .anyTimes();
-//    EasyMock.expect( mmiDiv.getSource() ).andReturn( source.getMethod( "div", Number.class, Number.class ) ).anyTimes();
-//    EasyMock.expect( mmiDiv.getReturnTypeMatchingInfo() ).andReturn( createMMI_N2N() )
-//        .anyTimes();
-//    EasyMock.expect( mmiDiv.getArgumentTypeMatchingInfos() )
-//        .andReturn(
-//            createMMIMap( createMMI_N2D(),
-//                createMMI_N2N() ) )
-//        .anyTimes();
-//
-//    MethodMatchingInfo mmiMult = EasyMock.createNiceMock( MethodMatchingInfo.class );
-//    EasyMock.expect( mmiMult.getTarget() )
-//        .andReturn( target.getDeclaredMethod( "mult", Number.class, Double.class ) )
-//        .anyTimes();
-//    EasyMock.expect( mmiMult.getSource() ).andReturn( source.getMethod( "mult", Number.class, Number.class ) )
-//        .anyTimes();
-//    EasyMock.expect( mmiMult.getReturnTypeMatchingInfo() ).andReturn( createMMI_D2N() )
-//        .anyTimes();
-//    EasyMock.expect( mmiMult.getArgumentTypeMatchingInfos() )
-//        .andReturn(
-//            createMMIMap( createMMI_N2N(),
-//                createMMI_N2D() ) )
-//        .anyTimes();
 		EasyMock.replay(mmiConcat, mmiAddInt, mmiAddGen, mmiGetLong);
-//    , mmiSub,
-//        mmiMult, mmiDiv );
-//
+
 		methodMatchingInfos.add(mmiConcat);
 		methodMatchingInfos.add(mmiAddInt);
 		methodMatchingInfos.add(mmiAddGen);
 		methodMatchingInfos.add(mmiGetLong);
-//    methodMatchingInfos.add( mmiDiv );
-//    methodMatchingInfos.add( mmiMult );
 
 		ModuleMatchingInfo<DesiredGenInterface> moduleMatchingInfo = EasyMock.createNiceMock(ModuleMatchingInfo.class);
 		EasyMock.expect(moduleMatchingInfo.getMethodMatchingInfos()).andReturn(methodMatchingInfos).anyTimes();
@@ -140,9 +107,84 @@ public class GenSpecSignatureMatchingTypeConverterTest {
 		assertThat(converted.getBoxedLongAttr(world_99_Spec), equalTo(99L));
 		assertThat(converted.getBoxedLongAttr(hello_2_Spec), equalTo(2L));
 
-//    assertThat( converted.add( 10, 2 ), equalTo( 3 ) );
-//    assertThat( converted.div( 4l, 2l ), equalTo( 2l ) );
-//    assertThat( converted.mult( 4l, 2l ), equalTo( 8l ) );
+		checkInvokationOfAllNonParametrizedMethods(converted);
+	}
+	
+	@Test
+	public void specSourceSpecTargetMatching() throws NoSuchMethodException, SecurityException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+		Class<DesiredSpecInterface> source = DesiredSpecInterface.class;
+		Class<OfferedSpecClass> target = OfferedSpecClass.class;
+		OfferedSpecClass convertationObject = new OfferedSpecClass();
+		SignatureMatchingTypeConverter<DesiredSpecInterface> converter = new SignatureMatchingTypeConverter<>(source);
+
+		Set<MethodMatchingInfo> methodMatchingInfos = new HashSet<>();
+
+		MethodMatchingInfo mmiConcat = EasyMock.createNiceMock(MethodMatchingInfo.class);
+		EasyMock.expect(mmiConcat.getTarget())
+				.andReturn(target.getDeclaredMethod("concat", Specific.class, Specific.class)).anyTimes();
+		EasyMock.expect(mmiConcat.getSource()).andReturn(source.getMethod("concat", Specific.class, Specific.class))
+				.anyTimes();
+		EasyMock.expect(mmiConcat.getReturnTypeMatchingInfo()).andReturn(createMMI_Str2Str()).anyTimes();
+		EasyMock.expect(mmiConcat.getArgumentTypeMatchingInfos())
+				.andReturn(createMMIMap(createMMI_S2S(), createMMI_S2S())).anyTimes();
+
+		MethodMatchingInfo mmiAddInt = EasyMock.createNiceMock(MethodMatchingInfo.class);
+		EasyMock.expect(mmiAddInt.getTarget()).andReturn(target.getDeclaredMethod("add", Specific.class, Integer.class))
+				.anyTimes();
+		EasyMock.expect(mmiAddInt.getSource()).andReturn(source.getMethod("add", Specific.class, Integer.class))
+				.anyTimes();
+		EasyMock.expect(mmiAddInt.getReturnTypeMatchingInfo()).andReturn(createMMI_S2S()).anyTimes();
+		EasyMock.expect(mmiAddInt.getArgumentTypeMatchingInfos())
+				.andReturn(createMMIMap(createMMI_S2S(), createMMI_Int2Int())).anyTimes();
+
+		MethodMatchingInfo mmiAddGen = EasyMock.createNiceMock(MethodMatchingInfo.class);
+		EasyMock.expect(mmiAddGen.getTarget())
+				.andReturn(target.getDeclaredMethod("add", Specific.class, Specific.class)).anyTimes();
+		EasyMock.expect(mmiAddGen.getSource()).andReturn(source.getMethod("add", Specific.class, Specific.class))
+				.anyTimes();
+		EasyMock.expect(mmiAddGen.getReturnTypeMatchingInfo()).andReturn(createMMI_S2S()).anyTimes();
+		EasyMock.expect(mmiAddGen.getArgumentTypeMatchingInfos())
+				.andReturn(createMMIMap(createMMI_S2S(), createMMI_S2S())).anyTimes();
+
+		MethodMatchingInfo mmiGetLong = EasyMock.createNiceMock(MethodMatchingInfo.class);
+		EasyMock.expect(mmiGetLong.getTarget()).andReturn(target.getDeclaredMethod("getBoxedLongAttr", Specific.class))
+				.anyTimes();
+		EasyMock.expect(mmiGetLong.getSource()).andReturn(source.getMethod("getBoxedLongAttr", Specific.class)).anyTimes();
+		EasyMock.expect(mmiGetLong.getReturnTypeMatchingInfo()).andReturn(createMMI_Long2Long()).anyTimes();
+		EasyMock.expect(mmiGetLong.getArgumentTypeMatchingInfos()).andReturn(createMMIMap()).anyTimes();
+
+		EasyMock.replay(mmiConcat, mmiAddInt, mmiAddGen, mmiGetLong);
+
+		methodMatchingInfos.add(mmiConcat);
+		methodMatchingInfos.add(mmiAddInt);
+		methodMatchingInfos.add(mmiAddGen);
+		methodMatchingInfos.add(mmiGetLong);
+
+		ModuleMatchingInfo<DesiredSpecInterface> moduleMatchingInfo = EasyMock.createNiceMock(ModuleMatchingInfo.class);
+		EasyMock.expect(moduleMatchingInfo.getMethodMatchingInfos()).andReturn(methodMatchingInfos).anyTimes();
+		EasyMock.replay(moduleMatchingInfo);
+
+		DesiredSpecInterface converted = converter.convert(convertationObject, moduleMatchingInfo);
+		Specific hello_true_Gen = new Specific("hello", true);
+		Specific world_true_Gen = new Specific("world", true);
+		Specific hello_2_false_Spec = new Specific("hello", 2);
+		Specific world_99_false_Spec = new Specific("world", 99);
+
+		assertThat(converted.concat(hello_true_Gen, world_true_Gen), equalTo("helloworld"));
+		assertThat(converted.concat(hello_2_false_Spec, world_99_false_Spec), equalTo("helloworld"));
+
+		General result_109 = converted.add(world_99_false_Spec, 10);
+		assertThat(result_109.getIntAttr(), equalTo(109));
+
+		General result_101 = converted.add(world_99_false_Spec, hello_2_false_Spec);
+		assertThat(result_101.getIntAttr(), equalTo(101));
+
+//		assertThat(converted.getBoxedLongAttr(world_5_44_Gen), equalTo(44L));
+//		assertThat(converted.getBoxedLongAttr(hello_true_Gen), equalTo(10L));
+//		assertThat(converted.getBoxedLongAttr(world_99_Spec), equalTo(99L));
+//		assertThat(converted.getBoxedLongAttr(hello_2_Spec), equalTo(2L));
+
 		checkInvokationOfAllNonParametrizedMethods(converted);
 	}
 
@@ -167,6 +209,15 @@ public class GenSpecSignatureMatchingTypeConverterTest {
 		ModuleMatchingInfo mmi = EasyMock.createNiceMock(ModuleMatchingInfo.class);
 		EasyMock.expect(mmi.getSource()).andReturn(String.class).anyTimes();
 		EasyMock.expect(mmi.getTarget()).andReturn(String.class).anyTimes();
+		EasyMock.expect(mmi.getMethodMatchingInfos()).andReturn(new HashSet<>()).anyTimes();
+		EasyMock.replay(mmi);
+		return mmi;
+	}
+	
+	private ModuleMatchingInfo createMMI_S2S() {
+		ModuleMatchingInfo mmi = EasyMock.createNiceMock(ModuleMatchingInfo.class);
+		EasyMock.expect(mmi.getSource()).andReturn(Specific.class).anyTimes();
+		EasyMock.expect(mmi.getTarget()).andReturn(Specific.class).anyTimes();
 		EasyMock.expect(mmi.getMethodMatchingInfos()).andReturn(new HashSet<>()).anyTimes();
 		EasyMock.replay(mmi);
 		return mmi;
@@ -250,48 +301,6 @@ public class GenSpecSignatureMatchingTypeConverterTest {
 		return mmit;
 	}
 
-//  @Test
-//  public void genTargetReturnTypeMatching() throws NoSuchMethodException, SecurityException, IllegalAccessException,
-//      IllegalArgumentException, InvocationTargetException {
-//    Class<InterfaceWrapper> source = InterfaceWrapper.class;
-//    Class<EnumNative> target = EnumNative.class;
-//    EnumNative convertationObject = EnumNative.CONSTANT_1;
-//    SignatureMatchingTypeConverter<InterfaceWrapper> converter = new SignatureMatchingTypeConverter<>( source );
-//
-//    Set<MethodMatchingInfo> methodMatchingInfos = new HashSet<>();
-//    MethodMatchingInfo methodMatchingInfoGetFalse = EasyMock.createNiceMock( MethodMatchingInfo.class );
-//    EasyMock.expect( methodMatchingInfoGetFalse.getTarget() ).andReturn( target.getMethod( "getFalse" ) ).anyTimes();
-//    EasyMock.expect( methodMatchingInfoGetFalse.getSource() ).andReturn( source.getMethod( "getFalse" ) ).anyTimes();
-//    methodMatchingInfos.add( methodMatchingInfoGetFalse );
-//
-//    MethodMatchingInfo methodMatchingInfoGetTrue = EasyMock.createNiceMock( MethodMatchingInfo.class );
-//    EasyMock.expect( methodMatchingInfoGetTrue.getTarget() ).andReturn( target.getMethod( "getTrue" ) ).anyTimes();
-//    EasyMock.expect( methodMatchingInfoGetTrue.getSource() ).andReturn( source.getMethod( "getTrue" ) ).anyTimes();
-//    methodMatchingInfos.add( methodMatchingInfoGetTrue );
-//
-//    MethodMatchingInfo methodMatchingInfoGetOne = EasyMock.createNiceMock( MethodMatchingInfo.class );
-//    EasyMock.expect( methodMatchingInfoGetOne.getTarget() ).andReturn( target.getMethod( "getOne" ) ).anyTimes();
-//    EasyMock.expect( methodMatchingInfoGetOne.getSource() ).andReturn( source.getMethod( "getOne" ) ).anyTimes();
-//    methodMatchingInfos.add( methodMatchingInfoGetOne );
-//
-//    ModuleMatchingInfo<InterfaceWrapper> moduleMatchingInfo = EasyMock.createNiceMock( ModuleMatchingInfo.class );
-//    EasyMock.expect( moduleMatchingInfo.getMethodMatchingInfos() ).andReturn( methodMatchingInfos ).anyTimes();
-//    EasyMock.replay( moduleMatchingInfo, methodMatchingInfoGetFalse, methodMatchingInfoGetTrue,
-//        methodMatchingInfoGetOne );
-//    InterfaceWrapper converted = converter.convert( convertationObject, moduleMatchingInfo );
-//    assertThat( converted.getFalse(), equalTo( convertationObject.getFalse() ) );
-//    assertThat( converted.getTrue(), equalTo( convertationObject.getTrue() ) );
-//    assertThat( converted.getOne(), equalTo( convertationObject.getOne() ) );
-//    checkInvokationOfAllNonParametrizedMethods( converted );
-//    // Check further calls
-//    checkBoolean2booleanCalls( converted.getFalse(), convertationObject.getFalse() );
-//
-//  }
-
-	private void checkBoolean2booleanCalls(Boolean spec, boolean false2) {
-		// TODO Auto-generated method stub
-
-	}
 
 	private void checkInvokationOfAllNonParametrizedMethods(Object converted)
 			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
