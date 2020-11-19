12e3f439313cb1068193819994657c52590e13e1
ngundermann <ngundermann@ws202247.nb.deg.local>
2020-11-19 06:50:29 +0100

    EJB-Container-Dummy
    Neue Tests eingefügt

---
+++ Quellcode/EJB-Container-Dummy/test/de/fernuni/hagen/ma/gundermann/ejb/FindingMinimalFoerderprogrammeProviderTest.java
@@ -0,0 +1,21 @@
+package de.fernuni.hagen.ma.gundermann.ejb;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.Test;
+
+import DE.data_experts.profi.profilcs.antrag2015.eler.ft.stammdaten.ejb.ElerFTStammdatenAuskunftService;
+import de.fernuni.hagen.ma.gundermann.ejb.beanimplementations.EftSTDAuskunftImpl;
+import de.fernuni.hagen.ma.gundermann.ejb.desiredinterfaces.MinimalFoerderprogrammeProvider;
+
+public class FindingMinimalFoerderprogrammeProviderTest {
+
+  @Test
+  public void findFullMatchingMinimalFoerderprogrammeProvider() {
+    Class<MinimalFoerderprogrammeProvider> desiredInterface = MinimalFoerderprogrammeProvider.class;
+    EJBContainer.CONTAINER.registerBean( ElerFTStammdatenAuskunftService.class, new EftSTDAuskunftImpl() );
+    MinimalFoerderprogrammeProvider desiredBean = new DesiredEJBFinder().getDesiredBean( desiredInterface );
+    assertThat( desiredBean, notNullValue() );
+  }
+}
