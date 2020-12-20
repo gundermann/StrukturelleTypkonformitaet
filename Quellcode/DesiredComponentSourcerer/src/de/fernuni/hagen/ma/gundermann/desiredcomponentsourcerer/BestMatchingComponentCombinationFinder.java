7469dd9131b685b9ab287cf901a63e3d5687278d
ngundermann <ngundermann@host.docker.internal>
2020-12-20 06:25:55 +0100

    DesiredComponentSourcerer
    - QuantitativeMatchRatingComparator ausgelagert

--- Quellcode/DesiredComponentSourcerer/src/de/fernuni/hagen/ma/gundermann/desiredcomponentsourcerer/BestMatchingComponentCombinationFinder.java
+++ Quellcode/DesiredComponentSourcerer/src/de/fernuni/hagen/ma/gundermann/desiredcomponentsourcerer/BestMatchingComponentCombinationFinder.java
@@ -2,25 +2,22 @@ package de.fernuni.hagen.ma.gundermann.desiredcomponentsourcerer;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-import de.fernuni.hagen.ma.gundermann.desiredcomponentsourcerer.heuristics.CombinationSelector;
 import de.fernuni.hagen.ma.gundermann.desiredcomponentsourcerer.heuristics.CommonSelector;
-import de.fernuni.hagen.ma.gundermann.desiredcomponentsourcerer.heuristics.SingleSelector;
+import de.fernuni.hagen.ma.gundermann.desiredcomponentsourcerer.heuristics.QuantitaiveMatchRatingComparator;
 import matching.modules.PartlyTypeMatchingInfo;
 
 // Auskommentierte Teile gehoeren zu Heuristiken, die ich begonnen hatte umzusetzten, aber nicht zuende gedacht hatte.
 public class BestMatchingComponentCombinationFinder {
 
   // Selektor für strukturell 100%ig matchende Komponenten
-  private final Selector singleSelector;
+  // private final Selector singleSelector;
 
   // Selektor für die Kombination von strukturell 100%ig matchenden Komponenten
-  private final Selector fullMatchingCombinationSelector;
+  // private final Selector fullMatchingCombinationSelector;
 
   // Selektor für die Kombination von Komponenten, sodass alle erwarteten Methoden bedient werden.
   private final Selector commonSelector;
@@ -33,11 +30,11 @@ public class BestMatchingComponentCombinationFinder {
       Map<Class<?>, PartlyTypeMatchingInfo> componentInterface2PartlyMatchingInfos ) {
     quantitativeSortedInfos = new ArrayList<>(
         componentInterface2PartlyMatchingInfos.values() );
-    Collections.sort( quantitativeSortedInfos, new QuantitaiveMatchRankingComparator() );
-    this.singleSelector = new SingleSelector( quantitativeSortedInfos.stream()
-        .filter( CombinationFinderUtils::isFullMatchingComponent ).collect( Collectors.toList() ) );
-    this.fullMatchingCombinationSelector = new CombinationSelector( quantitativeSortedInfos.stream()
-        .filter( CombinationFinderUtils::isFullMatchingComponent ).collect( Collectors.toList() ) );
+    Collections.sort( quantitativeSortedInfos, new QuantitaiveMatchRatingComparator() );
+    // this.singleSelector = new SingleSelector( quantitativeSortedInfos.stream()
+    // .filter( CombinationFinderUtils::isFullMatchingComponent ).collect( Collectors.toList() ) );
+    // this.fullMatchingCombinationSelector = new CombinationSelector( quantitativeSortedInfos.stream()
+    // .filter( CombinationFinderUtils::isFullMatchingComponent ).collect( Collectors.toList() ) );
     this.commonSelector = new CommonSelector( quantitativeSortedInfos );
   }
 
@@ -77,18 +74,6 @@ public class BestMatchingComponentCombinationFinder {
         return Optional.empty();
       }
     };
-  }
-
-  private static class QuantitaiveMatchRankingComparator implements Comparator<PartlyTypeMatchingInfo> {
-
-    // Der mit dem hoechsten Ranking soll vorne stehen
-    @Override
-    public int compare( PartlyTypeMatchingInfo o1, PartlyTypeMatchingInfo o2 ) {
-      Double ranking1 = Optional.ofNullable( o1 ).map( PartlyTypeMatchingInfo::getQuantitaiveMatchRating ).orElse( 0d );
-      Double ranking2 = Optional.ofNullable( o2 ).map( PartlyTypeMatchingInfo::getQuantitaiveMatchRating ).orElse( 0d );
-      return Double.compare( ranking2, ranking1 );
-    }
-
   }
 
 }
