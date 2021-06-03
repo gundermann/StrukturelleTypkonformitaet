55e86cdbbdff71a6ca741c90d5d3be07a9a32a39
ngundermann <ngundermann@host.docker.internal>
2021-06-03 09:10:34 +0200

    Matcherrating wird bei Multitargetproxies korrekt verwendet

--- Quellcode/DesiredComponentSourcerer/src/de/fernuni/hagen/ma/gundermann/desiredcomponentsourcerer/heuristics/AccumulatedMatchingRateComparator.java
+++ Quellcode/DesiredComponentSourcerer/src/de/fernuni/hagen/ma/gundermann/desiredcomponentsourcerer/heuristics/AccumulatedMatchingRateComparator.java
@@ -3,8 +3,10 @@ package de.fernuni.hagen.ma.gundermann.desiredcomponentsourcerer.heuristics;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import matching.MatcherRate;
+import matching.Setting;
 import matching.modules.PartlyTypeMatchingInfo;
 
 /**
@@ -15,15 +17,32 @@ public class AccumulatedMatchingRateComparator implements Comparator<Collection<
 
   @Override
   public int compare( Collection<PartlyTypeMatchingInfo> o1, Collection<PartlyTypeMatchingInfo> o2 ) {
-    Double ranking1 = Optional.ofNullable( o1 ).map( Collection::stream )
-        .map( s -> s.map( PartlyTypeMatchingInfo::getQualitativeMatchRating ).map( MatcherRate::getMatcherRating )
-            .reduce( 0d, ( a, b ) -> a + b ) )
-        .orElse( 0d );
+    Optional<Stream<MatcherRate>> mr1 = Optional.ofNullable( o1 ).map( Collection::stream )
+        .map( s -> s.map( PartlyTypeMatchingInfo::getQualitativeMatchRating ) );
 
-    Double ranking2 = Optional.ofNullable( o2 ).map( Collection::stream )
-        .map( s -> s.map( PartlyTypeMatchingInfo::getQualitativeMatchRating ).map( MatcherRate::getMatcherRating )
-            .reduce( 0d, ( a, b ) -> a + b ) )
-        .orElse( 0d );
+    Optional<Stream<MatcherRate>> mr2 = Optional.ofNullable( o2 ).map( Collection::stream )
+        .map( s -> s.map( PartlyTypeMatchingInfo::getQualitativeMatchRating ) );
+
+    if ( !mr1.isPresent() && !mr2.isPresent() ) {
+      return 0;
+    }
+    if ( mr1.isPresent() && !mr2.isPresent() ) {
+      return 1;
+    }
+    if ( !mr1.isPresent() && mr2.isPresent() ) {
+      return -1;
+    }
+
+    // Vorher
+    // Double ranking1 = mr1.get().map( MatcherRate::getMatcherRating ).reduce( 0d, ( a, b ) -> a + b );
+    // TODO
+    Double ranking1 = Setting.QUALITATIVE_COMPONENT_MATCH_RATE_CUMULATION.apply( mr1.get() ).getMatcherRating();
+
+    // Vorher
+    // Double ranking2 = mr2.get().map( MatcherRate::getMatcherRating ).reduce( 0d, ( a, b ) -> a + b );
+    // TODO
+    Double ranking2 = Setting.QUALITATIVE_COMPONENT_MATCH_RATE_CUMULATION.apply( mr2.get() ).getMatcherRating();
+
     return Double.compare( ranking1, ranking2 );
   }
 
