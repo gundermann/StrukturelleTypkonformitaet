c18e87ae0ea39e3caa17b12eef535c2b330e65ef
ngundermann <ngundermann@ws202247.nb.deg.local>
2021-11-08 09:57:46 +0100

    Methode ausgelagert

--- Quellcode/Signature Matching/src/de/fernuni/hagen/ma/gundermann/signaturematching/matching/MatcherCombiner.java
+++ Quellcode/Signature Matching/src/de/fernuni/hagen/ma/gundermann/signaturematching/matching/MatcherCombiner.java
@@ -11,57 +11,56 @@ import de.fernuni.hagen.ma.gundermann.signaturematching.SingleMatchingInfo;
 import de.fernuni.hagen.ma.gundermann.signaturematching.matching.types.TypeMatcher;
 
 public final class MatcherCombiner {
-  private MatcherCombiner() {
-  }
-
-  public static Supplier<TypeMatcher> combine( TypeMatcher... matcher ) {
-    return () -> new TypeMatcher() {
-
-      @Override
-      public boolean matchesType( Class<?> checkType, Class<?> queryType ) {
-
-        for ( TypeMatcher m : getSortedMatcher() ) {
-          if ( m.matchesType( checkType, queryType ) ) {
-            return true;
-          }
-        }
-        return false;
-      }
-
-      private Collection<TypeMatcher> getSortedMatcher() {
-        List<TypeMatcher> matcherList = Arrays.asList( matcher );
-        Collections.sort( matcherList,
-            ( l1, l2 ) -> Double.compare( l1.getTypeMatcherRate(), l2.getTypeMatcherRate() ) );
-        return matcherList;
-      }
-
-      @Override
-      public Collection<SingleMatchingInfo> calculateTypeMatchingInfos( Class<?> checkType, Class<?> queryType ) {
-        for ( TypeMatcher m : getSortedMatcher() ) {
-          if ( m.matchesType( checkType, queryType ) ) {
-            return m.calculateTypeMatchingInfos( checkType, queryType );
-          }
-        }
-        return new ArrayList<>();
-      }
-
-      @Override
-      public MatcherRate matchesWithRating( Class<?> checkType, Class<?> queryType ) {
-        for ( TypeMatcher m : getSortedMatcher() ) {
-          MatcherRate rating = m.matchesWithRating( checkType, queryType );
-          if ( rating != null ) {
-            return rating;
-          }
-        }
-        return null;
-      }
-
-	@Override
-	public double getTypeMatcherRate() {
-		//irrelevant, weil matchesWithRating ueberschrieben wurde.
-		return -0;
+	private MatcherCombiner() {
 	}
 
-    };
-  }
+	public static Supplier<TypeMatcher> combine(TypeMatcher... matcher) {
+		return () -> new TypeMatcher() {
+
+			@Override
+			public boolean matchesType(Class<?> checkType, Class<?> queryType) {
+				for (TypeMatcher m : getSortedMatcher()) {
+					if (m.matchesType(checkType, queryType)) {
+						return true;
+					}
+				}
+				return false;
+			}
+
+			@Override
+			public Collection<SingleMatchingInfo> calculateTypeMatchingInfos(Class<?> checkType, Class<?> queryType) {
+				for (TypeMatcher m : getSortedMatcher()) {
+					if (m.matchesType(checkType, queryType)) {
+						return m.calculateTypeMatchingInfos(checkType, queryType);
+					}
+				}
+				return new ArrayList<>();
+			}
+
+			@Override
+			public MatcherRate matchesWithRating(Class<?> checkType, Class<?> queryType) {
+				for (TypeMatcher m : getSortedMatcher()) {
+					MatcherRate rating = m.matchesWithRating(checkType, queryType);
+					if (rating != null) {
+						return rating;
+					}
+				}
+				return null;
+			}
+
+			@Override
+			public double getTypeMatcherRate() {
+				// irrelevant, weil matchesWithRating ueberschrieben wurde.
+				return -0;
+			}
+
+			private Collection<TypeMatcher> getSortedMatcher() {
+				List<TypeMatcher> matcherList = Arrays.asList(matcher);
+				Collections.sort(matcherList,
+						(l1, l2) -> Double.compare(l1.getTypeMatcherRate(), l2.getTypeMatcherRate()));
+				return matcherList;
+			}
+
+		};
+	}
 }
